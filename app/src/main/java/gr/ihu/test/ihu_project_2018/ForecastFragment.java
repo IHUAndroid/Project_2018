4db9a2ab2a7d1a8492c1d7b0d30cceb47dce98bb
teohaik <teohaik@gmail.com>
2018-04-18 22:48:21 +0300

    Forecast fragment with network code inside main error

---
+++ app/src/main/java/gr/ihu/test/ihu_project_2018/ForecastFragment.java
@@ -0,0 +1,124 @@
+package gr.ihu.test.ihu_project_2018;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ForecastFragment extends Fragment {
+
+    String[] data = {
+            "Mon 2/4 - Sunny - 30 ",
+            "Tue 3/4 - Foggy - 20",
+            "Wed 4/4 - Snow - 2",
+            "Thu 5/4 - Rain - 18",
+            "Fir 6/4 - Sunny - 32"
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main,
+                container,
+                false);
+
+        List<String> dataList = Arrays.asList(data);
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_text,
+                        dataList
+                );
+
+        ListView listView = (ListView) rootView.
+                findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+
+
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String forecastJsonStr = null;
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are avaiable at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+            //MODIFIED FOR CITY OF THESSALONIKI, GREECE
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?id=734077&mode=json&units=metric&cnt=7");
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            forecastJsonStr = buffer.toString();
+
+            Log.i("JSON_STRING", forecastJsonStr);
+
+        } catch (IOException e) {
+            Log.e("PlaceholderFragment", "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attemping
+            // to parse it.
+            return null;
+        } finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
+
+        return rootView;
+
+    }
+}
--- app/src/main/java/gr/ihu/test/ihu_project_2018/MainActivity.java
+++ app/src/main/java/gr/ihu/test/ihu_project_2018/MainActivity.java
@@ -1,18 +1,9 @@
 package gr.ihu.test.ihu_project_2018;
 
-        import android.support.annotation.Nullable;
-        import android.support.v4.app.Fragment;
+
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.ArrayAdapter;
-        import android.widget.ListView;
 
-        import java.util.ArrayList;
-        import java.util.Arrays;
-        import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,50 +15,10 @@ public class MainActivity extends AppCompatActivity {
         if(savedInstanceState == null){
             getSupportFragmentManager()
                     .beginTransaction()
-                    .add(R.id.activity_main, new PlaceholderFragment())
+                    .add(R.id.activity_main, new ForecastFragment())
                     .commit();
         }
 
-    }
-
-
-    public static class PlaceholderFragment extends Fragment {
-
-        String[] data = {
-                "Mon 2/4 - Sunny - 30 ",
-                "Tue 3/4 - Foggy - 20",
-                "Wed 4/4 - Snow - 2",
-                "Thu 5/4 - Rain - 18",
-                "Fir 6/4 - Sunny - 32"
-        };
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater,
-                                 @Nullable ViewGroup container,
-                                 @Nullable Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_main,
-                    container,
-                    false);
-
-            List<String> dataList = Arrays.asList(data);
-
-            ArrayAdapter<String> adapter =
-                    new ArrayAdapter<>(
-                            getActivity(),
-                            R.layout.list_item_forecast,
-                            R.id.list_item_forecast_text,
-                            dataList
-                    );
-
-            ListView listView = (ListView)rootView.
-                                findViewById(R.id.listview_forecast);
-            listView.setAdapter(adapter);
-
-            return rootView;
-
-        }
     }
 
 }
